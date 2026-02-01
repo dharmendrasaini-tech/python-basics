a650af76a8bdaaf57555d115942080e67386889e
Dharmendra Saini <dharmendrasaini.tech@gmail.com>
2026-02-01 15:32:25 +0530

    Completed 15 Python basics practice problems.

---
+++ practice_basics.py
@@ -0,0 +1,152 @@
+# Q1 Write a program to print your name, age, and city each on a new line.
+
+print("Dharmendra Saini")
+print("32")
+print("Jhalawar")
+
+# Q2 Store your age in a variable and print:   I am <age> years old.
+
+age = 32
+
+print("I am",age,"year old.")
+
+# Q3 Create two variables a = 10, b = 3 and print:  sum,difference,product,division result
+
+a = 10
+b = 3
+
+print("sum: ", a+b)
+print("difference: ", a-b)
+print("product: ", a* b)
+print("division: ", a / b)
+
+# Q4 Print the type of each: 10 , 10.5, "10", True
+
+print(type(10))
+print(type(10.5))
+print(type("10"))
+print(type(True))
+
+# Q5 Take user input for name and print: Hello <name>, welcome to Python
+
+name = input("Enter your name: ")
+
+print("Hello",name,"welcome to Python")
+
+# Q6 Take two numbers from the user and print their sum.
+
+a = int(input("Enter a number: "))
+b = int(input("Enter second number: "))
+
+print("Sum: ", a+b)
+
+# Q7 Take a number and print its square and cube.
+
+a = int(input("Enter a number: "))
+
+print("Square: ", a*a)
+print("Cube: ", a*a*a)
+
+# Q8 Take input for length and width of a rectangle and print the area.
+
+length = int(input("Length: "))
+width = int(input("Width: "))
+
+print("Area of a rectangle: ", length * width)
+
+# Q9 Take temperature in Celsius and convert it to Fahrenheit. (Formula: F = C × 9/5 + 32)
+
+temp = float(input("Enter temperature in celcius: "))
+print("Fahrenheit: ", (temp * 9/5) + 32)
+
+# Q10 Take two numbers using input() and ensure addition works numerically, not string concatenation.
+
+a = int(input("Enter a number: "))
+b = int(input("Enter second number: "))
+
+print("Addition: ", a+b)
+
+# Q11 Take a float number, convert it to int, and print both values.
+
+a = float(input("Enter a number: "))
+
+print("Float value: ",a, "Int value: ", int(a))
+
+# Q12 Ask the user for age and print its data type before and after converting it to int.
+
+age = input("Age: ")
+
+print(type(age), type(int(age)))
+
+# Q13 Take a number as input and print whether its type is int or float.
+
+a = input("Enter a number: ")
+
+print(type(a)) #The answer will be string.
+
+# Q14 Take two numbers and print: a > b , a < b , a == b
+
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+
+print(a>b)
+print(a<b)
+print(a==b)
+
+# Q14 Take a number as input and print the remainder when divided by 2.
+
+a = int(input("Enter a number: "))
+print(a % 2)
+
+# Q15 Input two numbers , print True if a is greater than or equal to b. If not print False.
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+print(a>=b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
